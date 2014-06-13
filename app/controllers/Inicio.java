beac2bb10e4f7d1c99a79a38bfc36621ac3f60ab
Gleyser Guimaraes <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2014-06-13 02:00:21 -0300

    Adicionando pagina inicial

--- app/controllers/Inicio.java
+++ app/controllers/Inicio.java
@@ -1,5 +1,10 @@
 package controllers;
 
-public class Inicio {
+import play.mvc.Controller;
+import play.mvc.Result;
 
+public class Inicio extends Controller{
+	public static Result index() {
+		return ok(views.html.inicio.render());
+	}
 }
