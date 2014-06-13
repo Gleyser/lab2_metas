f0ec41320c1cb5048b9ae5e80430947a422a1e74
Gleyser Guimaraes <gleyser.bonfim.guimaraes@ccc.ufcg.edu.br>
2014-06-13 00:56:02 -0300

    introduzindo o view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -10,5 +10,12 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
+    
+    public static Result sobre() {
+    	return ok(views.html.sobre.render(
+    			"Top 100 filmes Cult",
+    			play.core.PlayVersion.current())
+    			);
+    	}
 
 }
